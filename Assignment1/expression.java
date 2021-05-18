c2ef313f9454d0544df93d2b2e38d2ffaa4330a7
shivanijoshi2171998 <83664433+shivanijoshi2171998@users.noreply.github.com>
2021-05-18 22:40:33 +0530

    Add files via upload

---
+++ Assignment1/expression.java
@@ -0,0 +1,28 @@
+//fourth program
+/*public class expression
+{
+   public static void main(String args[])
+{
+   int i=-5+8*6;
+System.out.println(i);
+ int j=(55+9)%9;
+System.out.println(j);
+ int k=20+-3*5/8;
+System.out.println(k);
+int l=5+15/3*2-8%3;
+System.out.println(l);
+
+}
+}*/
+//second method
+public class expression
+{
+   public static void main(String args[])
+{
+System.out.println(-5+8*6);
+System.out.println((55+9)%9);
+System.out.println(20+-3*5/8);
+System.out.println(5+15/3*2-8%3);
+}
+}
+
