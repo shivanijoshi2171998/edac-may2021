376a75b11324585050848b3c1b659ce85d793532
shivanijoshi2171998 <83664433+shivanijoshi2171998@users.noreply.github.com>
2021-05-18 22:44:59 +0530

    Add files via upload

---
+++ Assignment1/sixteenth.java
@@ -0,0 +1,13 @@
+//sixteenth program
+class sixteenth
+{
+    public static void main(String args[])
+{
+     System.out.println("+'  '  '  '+");
+    System.out.println("[|  o o    |]");
+     System.out.println("|    ^     |");
+     System.out.println("|   -_-    |");
+     System.out.println("+- - - - -+");
+}
+
+}
