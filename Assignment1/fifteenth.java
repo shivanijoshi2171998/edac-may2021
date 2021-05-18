3d819c9b8a850ba83776261068b72cd1e4cc1ee1
shivanijoshi2171998 <83664433+shivanijoshi2171998@users.noreply.github.com>
2021-05-18 22:41:05 +0530

    Add files via upload

---
+++ Assignment1/fifteenth.java
@@ -0,0 +1,23 @@
+//fifteenth program
+import java.util.Scanner;
+public class fifteenth
+{
+   public static void main(String args[])
+{
+   Scanner sc=new Scanner(System.in);
+    System.out.println("enter 2 num");
+   int i=sc.nextInt();
+   int j=sc.nextInt();
+    int temp;
+   temp=i;
+   i=j;
+   j=temp;
+
+  System.out.println("swapping=");
+  System.out.println("i="+i);
+System.out.println("j="+j);
+
+  
+  
+}
+}
