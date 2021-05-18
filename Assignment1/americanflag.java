99b8674e2999ea0b6ece8987684895e3a29fe0c9
shivanijoshi2171998 <83664433+shivanijoshi2171998@users.noreply.github.com>
2021-05-18 22:36:49 +0530

    Add files via upload

---
+++ Assignment1/americanflag.java
@@ -0,0 +1,24 @@
+class americanflag
+{
+  public static void main(String args[])
+  {
+    for(int i=1;i<=15;i++)
+	{
+	  if(i<=9)
+	  {
+	    if(i%2==1)
+		{
+		  System.out.println("* * * * * * = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = ");
+		}
+		else
+		{
+		  System.out.println(" * * * * *  = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =");
+		}
+	  }
+	  else
+	  {
+	    System.out.println("= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =  ");
+	  }
+	}
+  }
+}
