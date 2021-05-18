dc6129ba6b7c06fd33b7a62e2f1e954121f7cdc0
shivanijoshi2171998 <83664433+shivanijoshi2171998@users.noreply.github.com>
2021-05-18 22:44:27 +0530

    Add files via upload

---
+++ Assignment1/second.java
@@ -0,0 +1,27 @@
+//second program
+/*import java.util.Scanner;
+class second
+{
+ public static void main(String args[])
+{
+   Scanner sc=new Scanner(System.in);
+  int i=sc.nextInt();
+  int j=sc.nextInt();
+  int k=i+j;
+
+  System.out.println("addition:"+k);
+}
+}*/
+
+//second method to print addition without taking input from user
+class second
+{
+   public static void main(String args[])
+{
+   int a=74;
+int b=36;
+int k=a+b;
+ 
+  System.out.println("addition:"+k);
+}
+}
