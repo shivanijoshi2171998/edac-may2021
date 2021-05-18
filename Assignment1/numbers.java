b60db24215705d9355da0fe74cc6d10a95e8db1c
shivanijoshi2171998 <83664433+shivanijoshi2171998@users.noreply.github.com>
2021-05-18 22:42:48 +0530

    Add files via upload

---
+++ Assignment1/numbers.java
@@ -0,0 +1,27 @@
+//seventh program = print a table of 8
+/*import java.util.Scanner;
+public class numbers
+{
+   public static void main(String args[])
+{
+    Scanner sc=new Scanner(System.in);
+   int num=sc.nextInt();
+   for(int i=0; i<10; i++)
+{
+    System.out.println(num + "x" +(i+1) +"=" +(num *(i+1)));
+}
+}
+}*/
+
+// second method to print table
+public class numbers
+{
+  public static void main(String args[])
+{
+   int num=8;
+for(int i=1; i<=10; ++i)
+{
+   System.out.print("%d * %d=%d\n",num, i, num*i);
+}
+}
+}
