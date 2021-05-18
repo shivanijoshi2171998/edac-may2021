13f47cba944d2c7e68f6bfce4a6415ee24ab6663
shivanijoshi2171998 <83664433+shivanijoshi2171998@users.noreply.github.com>
2021-05-18 22:46:19 +0530

    Add files via upload

---
+++ Assignment1/twelth.java
@@ -0,0 +1,15 @@
+//twelth program
+import java.util.Scanner;
+public class twelth
+{
+   public static void main(String args[])
+{
+    Scanner sc=new Scanner(System.in);
+    int i=sc.nextInt();
+    int j=sc.nextInt();
+    int k=sc.nextInt();
+    int l=(i+j+k)/3;
+
+    System.out.println("avarage:"+l);
+}
+}
