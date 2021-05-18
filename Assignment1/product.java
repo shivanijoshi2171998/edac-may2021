0120e5636b4868b0cd8751d840f0754ab612e04e
shivanijoshi2171998 <83664433+shivanijoshi2171998@users.noreply.github.com>
2021-05-18 22:43:58 +0530

    Add files via upload

---
+++ Assignment1/product.java
@@ -0,0 +1,32 @@
+//fifth program
+/*import java.util.Scanner;
+public class product
+{
+   public static void main(String args[])
+{
+    Scanner sc=new Scanner(System.in);
+    int i=sc.nextInt();
+    int j=sc.nextInt();
+    int k=i*j;
+
+   System.out.println("product:"+k);
+}
+}*/
+
+//second method not run the program
+public class product
+{
+  public static void main(String args[])
+{
+String s1=args[0];
+ String s2=args[1];
+   int i=Integer.parseInt(s1);
+   int j=Integer.parseInt(s2);
+   int k=i*j;
+
+  System.out.println("enter first number:"+i);
+  System.out.println("enter third number:"+j);
+  System.out.println("product:"+k);
+
+}
+}
