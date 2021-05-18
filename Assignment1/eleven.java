10b2f0c2ee4c1144eb44df872da3ad08e9bccb20
shivanijoshi2171998 <83664433+shivanijoshi2171998@users.noreply.github.com>
2021-05-18 22:39:26 +0530

    Add files via upload

---
+++ Assignment1/eleven.java
@@ -0,0 +1,13 @@
+//eleventh  program
+public class eleven
+{
+   public static void main(String args[])
+{
+    System.out.println("perimeter of circle is:");
+    double i=2*Math.PI*7.5D;
+    System.out.println(i);
+   System.out.println("area of circle is:");
+   double a=Math.PI*7.5D*7.5D;
+   System.out.println(a);
+}
+}
