3cf89232129209a18f4ebcbe1d7aa03f6e29ed87
shivanijoshi2171998 <83664433+shivanijoshi2171998@users.noreply.github.com>
2021-06-15 02:20:17 +0530

    Add files via upload

---
+++ Assignment2/letterpattern18.java
@@ -0,0 +1,16 @@
+class letterpattern18
+{
+  public static void main(String args[])
+  {
+    int alpha=64;
+    for(int i=5;i>=1;i--)
+	{
+	   for(int j=1;j<=i;j++)
+	   {
+	      System.out.print((char)(alpha+j)+" ");
+		  
+	   }
+	   System.out.println();
+	}
+  }
+}
