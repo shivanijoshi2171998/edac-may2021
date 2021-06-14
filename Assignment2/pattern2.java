813daf14022629bd9e3df1c5a3367802bc63cf97
shivanijoshi2171998 <83664433+shivanijoshi2171998@users.noreply.github.com>
2021-06-15 02:26:05 +0530

    Add files via upload

---
+++ Assignment2/pattern2.java
@@ -0,0 +1,15 @@
+//que 3
+class pattern2
+{
+  public static void main(String args[])
+  {
+     for(int i=1;i<=5;i++)
+	 {
+	   for(int j=5;j>=i;j--)
+	   {
+	     System.out.print("*");
+	   }
+	      System.out.println();
+	 }
+  }
+}
