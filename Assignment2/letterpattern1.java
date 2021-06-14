cced6f51e2dbdcdd314fdc917b3d835be43609d6
shivanijoshi2171998 <83664433+shivanijoshi2171998@users.noreply.github.com>
2021-06-15 02:18:50 +0530

    Add files via upload

---
+++ Assignment2/letterpattern1.java
@@ -0,0 +1,18 @@
+//letter pattern
+class letterpattern1
+{
+  public static void main(String args[])
+  {
+  int alpha=64;
+  {
+    for(int i=1;i<=6;i++)
+	{
+	  for(int j=1;j<=i;j++)
+	  {
+	    System.out.print((char)(alpha+j)+" ");
+	  }
+	  System.out.println();
+	}
+  }
+  }
+}
