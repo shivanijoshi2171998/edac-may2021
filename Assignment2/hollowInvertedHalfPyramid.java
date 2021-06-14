6d583c9214bd592849c683e7517de5d7b1f2f8cc
shivanijoshi2171998 <83664433+shivanijoshi2171998@users.noreply.github.com>
2021-06-15 02:15:59 +0530

    Add files via upload

---
+++ Assignment2/hollowInvertedHalfPyramid.java
@@ -0,0 +1,18 @@
+class hollowInvertedHalfPyramid
+{
+  public static void main(String args[])
+{
+  for(int i=1;i<=5;i++)
+  {
+     for(int j=5;j>=i;j--)
+	 {
+	   if(j==i || j==5 || i==1)
+	    System.out.print("*");
+		
+	   else
+        System.out.print(" ");	   
+	 }
+	 System.out.println();
+  }
+}  
+}
