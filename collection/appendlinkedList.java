2c6a840f452856bc066ebc38b087385bb026fff6
shivanijoshi2171998 <83664433+shivanijoshi2171998@users.noreply.github.com>
2021-06-21 19:53:23 +0530

    Add files via upload

---
+++ collection/appendlinkedList.java
@@ -0,0 +1,17 @@
+//1. Write a Java program to append the specified element to the end of a linked list.
+
+import java.util.*;
+public class appendlinkedList
+{
+   public static void main(String args[])
+{
+    LinkedList<String> list=new LinkedList<>();
+	list.add("apple");
+	list.add("mango");
+	list.add("grapes");
+	list.add("oranges");
+	
+	System.out.println(list);
+	
+}   
+}
